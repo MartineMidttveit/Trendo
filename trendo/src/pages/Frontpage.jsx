3b30fed3aeb537f9954a5372f298841ff2691321
Martine23FT <martinemidttveit@hotmail.com>
2024-09-04 13:06:48 +0200

    Changes

--- trendo/src/pages/Frontpage.jsx
+++ trendo/src/pages/Frontpage.jsx
@@ -9,15 +9,27 @@ function Frontpage() {
       <main className="text-primary">
         <Header/>
         <div className="w-full bg-cover bg-center relative top-[145px]" style={{ backgroundImage: `url('/background.png')` }}>
-          <div className="wrap bg-customGrey shadow-lg">
+            <div className="wrap bg-customGrey shadow-lg">
 
-            <div className='pt-10 text-left w-5/6 mx-auto'>
-              <h1 className='text-2xl font-bold'>All products</h1>
-              <p className='2xl:text-lg pt-2'>Showing <span className='font-medium'>11</span> out of <span className='font-medium'>11.345</span> products</p>
-            </div>
+              <div className='flex items-end wrap'>
+                <div className='pt-10 text-left w-5/6 mx-auto'>
+                  <h1 className='text-2xl font-bold'>All products</h1>
+                  <p className='2xl:text-lg pt-2'>Showing <span className='font-medium'>11</span> out of <span className='font-medium'>11.345</span> products</p>
+                </div>
+
+                <div class="relative sm:w-64">
+                    <select name="sortBy" id="sortBy" class="text-sm xl:text-base border rounded px-3 py-2 outline-none h-10 w-full sm:w-64 xl:h-12" aria-label="Sort listings">
+                        <option value="Latest">Most popular</option>
+                        <option value="Ending">Newest products</option>
+                        <option value="TitleAZ">Lowest price</option>
+                        <option value="TitleZA">Highest price</option>
+                        <option value="TitleZA">A to Z</option>
+                    </select>
+                </div>
+              </div>
             
-            <Cards/>
-          </div>
+              <Cards/>
+            </div>
           <Footer/>
         </div>
       </main>
