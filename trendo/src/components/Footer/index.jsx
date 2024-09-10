44f44f008cf88add686c204cf3e0633b27cbb769
Martine23FT <martinemidttveit@hotmail.com>
2024-09-10 15:42:04 +0200

    Changes

--- trendo/src/components/Footer/index.jsx
+++ trendo/src/components/Footer/index.jsx
@@ -4,30 +4,30 @@ import Newsletter from './Newsletter';
 function Footer() {
     return (
         <footer className='bg-customOrange py-6'>
-            <div className='wrapper flex justify-between mx-auto w-full'>
-                <h3 className='text-left 2xl:text-xl font-bold'>Trendo</h3>
+            <div className='wrapper flex flex-col lg:flex-row lg:justify-between mx-auto w-full'>
+                <h3 className='text-left text-lg 2xl:text-xl font-bold my-4 lg:my-0'>Trendo</h3>
 
-                <div className='flex w-1/2 justify-between text-left'>
+                <div className='flex w-full lg:w-1/2 flex-col lg:flex-row lg:justify-between text-left'>
 
-                    <div className='flex flex-col gap-3'>
+                    <div className='flex flex-col gap-1.5 lg:gap-3 mb-5'>
                         <h4 className='font-medium 2xl:text-lg'>Terms</h4>
-                        <p>Copyright © 2024 Trendo</p>
-                        <p>Terms & conditions</p>
-                        <p>Privacy policy</p>
+                        <p className='text-sm lg:text-base'>Copyright © 2024 Trendo</p>
+                        <p className='text-sm lg:text-base'>Terms & conditions</p>
+                        <p className='text-sm lg:text-base'>Privacy policy</p>
                     </div>
 
-                    <div className='flex flex-col gap-3'>
+                    <div className='flex flex-col gap-1.5 lg:gap-3 mb-5'>
                         <h4 className='font-medium 2xl:text-lg'>Contact</h4>
-                        <p>Contact us</p>
-                        <p>FAQ</p>
-                        <p>Phone: +47 65 76 78 76</p>
-                        <p>Mail: <span className='underline'>support@trendo.com</span></p>
+                        <p className='text-sm lg:text-base'>Contact us</p>
+                        <p className='text-sm lg:text-base'>FAQ</p>
+                        <p className='text-sm lg:text-base'>Phone: +47 65 76 78 76</p>
+                        <p className='text-sm lg:text-base'>Mail: <span className='underline'>support@trendo.com</span></p>
                     </div>
 
-                    <div className='flex flex-col'>
-                        <h4 className='font-medium 2xl:text-lg pb-3'>Newsletter</h4>
-                        <p>Join the Trendo-club to get exclusive</p>
-                        <p>offers and get updated about latest trends.</p>
+                    <div className='flex flex-col gap-1.5 lg:gap-3 mb-5'>
+                        <h4 className='font-medium 2xl:text-lg lg:pb-3'>Newsletter</h4>
+                        <p className='text-sm lg:text-base'>Join the Trendo-club to get exclusive</p>
+                        <p className='text-sm lg:text-base'>offers and get updated about latest trends.</p>
 
                         <Newsletter/>
                     </div>
--- trendo/src/pages/Frontpage.jsx
+++ trendo/src/pages/Frontpage.jsx
@@ -14,25 +14,41 @@ const {products, setProducts} = useContext(productContext)
       <main className="text-primary font-barlow relative">
             <div className="bg-customGrey shadow-lg">
 
-              <div className="flex justify-center relative">
+              <div className="flex justify-center items-center relative">
                 <img src="/image.png" alt="Woman with sunglasses in front of a wall of leaves and flowers" className="w-full object-cover h-screen"/>
 
-                <div className='absolute left-0 mt-16 top-1/3 text-customGrey text-left wrapper'>
-                  <p className='text-2xl font-medium mb-5'>SAVE UP TO 20% EXTRA ON THE</p>
-                  <h1 className='text-8xl font-medium'>Summer sale</h1>
-                  <p className='text-3xl mt-8'>Don't miss out — shop now for the best deals!</p>
+                <div className='absolute left-0 xl:mt-16 xl:top-1/3 text-customGrey text-left wrapper bg-black bg-opacity-60 h-screen w-screen flex flex-col justify-center xl:bg-transparent xl:block xl:h-auto'>
+                  <p className='text-lg xl:text-2xl font-medium mb-2 xl:mb-5 pt-28'>SAVE UP TO 20% EXTRA ON THE</p>
+                  <h1 className='text-4xl xl:text-8xl font-bold xl:font-medium'>Summer sale</h1>
+                  <p className='xl:text-3xl mt-3 2xl:mt-8'>Don't miss out — shop now for the best deals!</p>
 
-                  <button className='bg-customOrange 2xl:px-8 2xl:py-3 2xl:mt-10 text-primary font-medium 2xl:text-lg hover:bg-customGrey hover:text-customGreen duration-300'>SHOP NOW</button>
+                  <button 
+                    className='
+                    bg-customOrange 
+                    mt-4 
+                    w-fit
+                    py-2.5
+                    px-6
+                    text-sm
+                    2xl:px-8 
+                    2xl:py-3 
+                    2xl:mt-10 
+                    text-primary 
+                    font-medium 
+                    2xl:text-lg 
+                    hover:bg-customGrey 
+                    hover:text-customGreen 
+                    duration-300'>SHOP NOW</button>
                 </div>
               </div>
 
-              <div className='flex items-end wrapper'>
+              <div className='flex lg:items-end flex-col lg:flex-row wrapper'>
                 <div className='pt-10 text-left w-full mx-auto'>
                   <h1 className='text-2xl font-bold'>All products</h1>
                   <p className='2xl:text-lg pt-2'>Showing <span className='font-medium'>11</span> out of <span className='font-medium'>11.345</span> products</p>
                 </div>
 
-                <div className="relative sm:w-64">
+                <div className="relative sm:w-64 mt-4">
                     <select name="sortBy" id="sortBy" className="text-sm xl:text-base border rounded px-3 py-2 outline-none h-10 w-full sm:w-64 xl:h-12" aria-label="Sort listings">
                         <option value="Latest">Most popular</option>
                         <option value="Ending">Newest products</option>
